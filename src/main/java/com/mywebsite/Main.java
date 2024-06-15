8522fc2ba0f1c17db83e669b7355b95f2bb4239e
Kasperx <titango2011@gmail.com>
2024-06-15 16:33:50 +0200

    data-input: driving distance

--- src/main/java/com/mywebsite/Main.java
+++ src/main/java/com/mywebsite/Main.java
@@ -5,6 +5,9 @@ import main.java.com.mywebsite.database.DAO.Dao_DBConnect;
 import main.java.com.mywebsite.database.Database;
 import main.java.com.mywebsite.database.DatabaseSQLite;
 import main.java.com.mywebsite.service.Tools;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,27 +28,65 @@ public class Main extends Dao_DBConnect{
             database.insertData(fileSrcData, true);
         }
 
-        for(String arg: args){
-            if(arg.equalsIgnoreCase("-help")
-                    || arg.equalsIgnoreCase("-h")
-                    || arg.equalsIgnoreCase("-?")
-                    || arg.equalsIgnoreCase("?")){
+        int expectedDrivingDistance;
+
+        //for(String arg: args){
+        for(int i=0; i<args.length; i++){
+            if(args[i].equalsIgnoreCase("-help")
+                    || args[i].equalsIgnoreCase("-h")
+                    || args[i].equalsIgnoreCase("-?")
+                    || args[i].equalsIgnoreCase("?")){
                     showHelp();
-            }
-            if(arg.equalsIgnoreCase("-print")
-                    || arg.equalsIgnoreCase("print-info")){
+            } else if(args[i].equalsIgnoreCase("-print")
+                    || args[i].equalsIgnoreCase("print-info")){
                 database.printInfo();
-            }
-            if(arg.equalsIgnoreCase("-print-data")){
+            } else if(args[i].equalsIgnoreCase("-print-data")){
                 database.printData();
-            }
-            if(arg.equalsIgnoreCase("-print-all")){
+            } else if(args[i].equalsIgnoreCase("-print-all")){
                 database.printAllData();
+            } if(args[i].equalsIgnoreCase("-distance")){
+                if(isLastPosition(args, i)) {
+                    logger.info("Number for expected driving distance missing.");
+                    System.exit(1);
+                } else {
+                    if (NumberUtils.isDigits(args[i + 1])) {
+                        expectedDrivingDistance = Integer.parseInt(args[i + 1]);
+                        logger.info("Expected driving distance: " + expectedDrivingDistance);
+                        if (expectedDrivingDistance < 0) {
+                            logger.info("Error: Number must be greater than 0.");
+                        } else {
+                            float calcFactor = (float)
+                                    expectedDrivingDistance > 0 && expectedDrivingDistance <= 5000
+                                    ? 0.5f
+                                    : expectedDrivingDistance > 5000 && expectedDrivingDistance <= 10000
+                                    ? 1.0f
+                                    : expectedDrivingDistance > 10000 && expectedDrivingDistance <= 20000
+                                    ? 1.5f
+                                    : 2.0f;
+                            logger.info("Calculated factor: " + calcFactor);
+                        }
+                    }
+                }
             }
         }
         logger.info("Bye");
         System.exit(0);
     }
+
+    static boolean isLastPosition(String[] array, int position){
+        return position == (array.length-1);
+        /*
+        for(int i=0; i<array.length; i++){
+            if(array[i].equals(parameter)){
+                if(i == (array.length - 1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+         */
+    }
+
     private static void showHelp (){
         logger.info("");
         logger.info("### This program is a test program for filtering location data and deciding some insurance parameter with those data ###");
@@ -56,6 +97,7 @@ public class Main extends Dao_DBConnect{
         logger.info("\t\t -print | print-info   print info of stored data");
         logger.info("\t\t -print-data           print data from database (with limit "+LIMIT_PRINT_DATA+")");
         logger.info("\t\t -print-all            print data from database");
+        logger.info("\t\t -distance             input driven distance per year in km as next parameter with [0-9]{1-10000}");
         logger.info("\nBye");
         System.exit(0);
     }
